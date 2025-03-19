865c75c1a61b137887089b47a8f336810a1b1b1e
Hossein Fazel <fazel.hosein9654@gmail.com>
2025-03-19 21:44:30 +0330

    add send file feature

--- client/client.go
+++ client/client.go
@@ -28,14 +28,54 @@ func StartClient(wg *sync.WaitGroup, server string, port int, name string) {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		message := scanner.Text()
+		message = strings.TrimSpace(message)
 		if strings.ToLower(message) == "exit"{
 			wg.Done()
+		}else if len(message) >= 6 && message[:6] == "<SEND " {
+			fp := message[6: len(message)-1]
+			finfo, err := os.Stat(fp)
+			if err != nil{
+				fmt.Println(err)
+				continue
+			}
+			send_file(finfo, fp, conn)
 		}else{
-			_, err := conn.Write([]byte(fmt.Sprintf("%v : ", name) + message + "\n"))
+			_, err := conn.Write([]byte(fmt.Sprintf("%v : %v\n", name, message)))
 			if err != nil {
 				fmt.Println("Error sending message:", err)
 				return
 			}
 		}
 	}
+}
+
+func send_file(file_info os.FileInfo, file_path string, conn net.Conn){
+	var header string = fmt.Sprintf("<RECEIVE:%v:%v>\n", file_info.Name(), file_info.Size())
+
+	_, err := conn.Write([]byte(header))
+	if err != nil{
+		fmt.Println("Error sending file:", err)
+		return
+	}
+	file, _ := os.Open(file_path)
+	defer file.Close()
+	buffer := make([]byte, 4096)
+	for {
+		count, err := file.Read(buffer)
+		if err != nil{
+			if count == 0{
+				conn.Write([]byte("<END>"))
+				break
+			}
+			fmt.Println("An error occurred")
+			return
+		}
+
+		_, err = conn.Write(buffer[:count])
+		if err != nil {
+			fmt.Println("Error sending file:", err)
+			return
+		}
+	}
+	fmt.Printf("File '%v' sent successfully.\n", file_info.Name())
 }
--- server/server.go
+++ server/server.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"net"
+	"os"
 	"strings"
 )
 
@@ -17,7 +18,34 @@ func handleConnection(conn net.Conn) {
 			return
 		}
 		message = strings.TrimSpace(message)
-		fmt.Printf("%s\n", message)
+		if len(message) >= 9 && message[:9] == "<RECEIVE:"{
+			parts := strings.Split(message, ":")
+		
+			file, err := os.Create(parts[1])
+			if err != nil{
+				fmt.Printf("An error occurred: %v\n", err)
+			}
+			defer file.Close()
+			
+			fmt.Printf("Receiving file '%v' (%v bytes)\n", parts[1], parts[2][:len(parts[2])-1])
+			buffer := make([]byte, 4096)
+			for {
+				count, err := reader.Read(buffer)
+				if err != nil{
+					fmt.Println("An error occurred")
+					return
+				}
+				if string(buffer[:count]) == "<END>"{
+					break
+				}
+
+				file.Write(buffer[:count])
+			}
+			fmt.Printf("File '%v' received successfully.\n", parts[1])
+
+		}else{
+			fmt.Printf("%s\n", message)
+		}
 	}
 }
 
@@ -28,7 +56,7 @@ func StartServer(server string, port int) {
 		return
 	}
 	defer listener.Close()
-	fmt.Printf("Server started on %v:%v", server, port)
+	fmt.Printf("Server started on %v:%v\n", server, port)
 
 	for {
 		conn, err := listener.Accept()
